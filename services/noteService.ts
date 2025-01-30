087cc197d8629a5d1292b423d971de4d40539183
Darius Kassi <kassidarius@gmail.com>
2025-01-30 03:23:33 +0000

    feat(notes): improve note fetching logic to handle homework and class-specific queries

--- services/noteService.ts
+++ services/noteService.ts
@@ -3,51 +3,46 @@ import { NOTE_TYPE, supabase } from "@/lib/supabase";
 
 export const note = {
   async getNotes(studentId: string, noteType: NOTE_TYPE[]): Promise<INoteDTO[]> {
+    const isHomework = noteType.length === 1 && noteType[0] === NOTE_TYPE.HOMEWORK;
+
     try {
+      let classId = isHomework ? (await supabase.from('students').select('class_id').eq('id', studentId).single().throwOnError()).data?.class_id : null;
       let query = supabase
-        .from('note_details')
+        .from('notes')
         .select(`
-          id,
-          note,
-          graded_at,
-          notes!inner (
-            id,
-            created_at,
-            due_date,
-            is_graded,
-            subjects!inner (
-              id,
-              name
-            )
-          )
-        `)
+          id, is_graded, due_date, created_at,
+          subjects!inner (id, name),
+          details:note_details (id, note, graded_at, student_id)
+          `)
         // TODO: filter notes by selected month from schoolYear
         // .eq('notes.school_year_id', yearId)
         // .eq('notes.semester_id', semesterId)
-        .eq('student_id', studentId)
-        .eq('notes.is_active', true)
-        .eq('notes.is_published', true)
-  
-      if (noteType.length > 1) {
-        query = query.in('notes.note_type', noteType)
-      } else {
-        query = query.eq('notes.note_type', noteType[0])
-      }
+        .eq('is_active', true)
+        .eq('is_published', true)
+        
+        if (classId) {
+          query = query
+            .eq('class_id', classId)
+            .eq('note_type', noteType[0])
+            .gte('due_date', new Date())
+        } else {
+          query = query
+            .eq('note_details.student_id', studentId)
+            .in('note_type', noteType)
+        }
   
       const { data, error } = await query;
   
       if (error) throw new Error(error.message);
   
-      return data.map((noteDetail) => ({
-        id: noteDetail.id,
-        subjectId: noteDetail.notes.subjects.id,
-        subjectName: noteDetail.notes.subjects.name,
-        note: noteDetail.note ?? 0,
-        date: new Date(noteDetail.notes.created_at),
-        dueDate:  noteDetail.notes.due_date
-        ? new Date(noteDetail.notes.due_date)
-        : null,
-        isGraded: noteDetail.notes.is_graded
+      return data.map((dt) => ({
+        id:  classId ? dt.id : dt.details[0].id,
+        subjectId: dt.subjects.id,
+        subjectName: dt.subjects.name,
+        note: classId ? 0 : dt.details[0].note ?? 0,
+        date: new Date(dt.created_at),
+        dueDate: dt.due_date ? new Date(dt.due_date) : null,
+        isGraded: dt.is_graded,
       } satisfies INoteDTO));
     } catch (error) {
       console.error("Error getting note records:", error);
