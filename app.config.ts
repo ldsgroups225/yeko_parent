6123877d059bb49096fba9a7bf23ef243965b3a8
Darius Kassi <kassidarius@gmail.com>
2025-02-21 00:07:13 +0000

    fix: update splash image path in app configuration

--- app.config.ts
+++ app.config.ts
@@ -30,7 +30,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     favicon: "./assets/images/favicon.png",
   },
   splash: {
-    image: "./assets/images/splash.png",
+    image: "./assets/images/splash-icon.png",
     resizeMode: "contain",
     backgroundColor: "#ffffff",
   },
