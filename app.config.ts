94c57e9fcdbf62ce206657949c0cfda61a3aa277
Darius Kassi <kassidarius@gmail.com>
2025-08-29 21:51:21 +0000

    chore: change the project scheme

--- app.config.ts
+++ app.config.ts
@@ -8,7 +8,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   version: "1.0.0",
   orientation: "portrait",
   icon: "./assets/images/icon.png", // recommended 1024x1024 png
-  scheme: "io.ldsgroups.yeko",
+  scheme: "yeko-parent",
   userInterfaceStyle: "automatic",
   newArchEnabled: true,
   ios: {
